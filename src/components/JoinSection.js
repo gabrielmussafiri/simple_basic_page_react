de4185a40759cf1b2c3751bf20b4a15a67a44c7d
gabrielmussafiri <musgabriel2@gmail.com>
2023-05-07 02:10:30 +0200

    JoinSection component created

---
+++ src/components/JoinSection.js
@@ -0,0 +1,22 @@
+function JoinSection() {
+  return (
+    <section class="join">
+      <h2> Join Today!</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit molestias
+        a ea modi rem pariatur exercitationem voluptatum harum repellat delectus
+        laudantium, beatae facilis blanditiis quos quae fugiat quam veritatis
+        eaque.
+      </p>
+      <form action="">
+        <input
+          type="email"
+          name="email"
+          placeholder="Type Email here"
+          required
+        />
+      </form>
+    </section>
+  );
+}
+export default JoinSection;
